3483e2593da5ea842120ff0100711c19403ab084
17669302192 <qm17669302192@163.com>
2026-04-14 18:41:11 +0800

    initial commit: nextjs blog project with prolayout

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // !! 警告：仅用于排查。这会让 Vercel 忽略类型错误直接发布
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
